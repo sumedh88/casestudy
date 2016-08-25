00e7aa5eba3b5026236ebee56af46a1a0f723215
sumedh88 <sumedh_surlekar@persistent.com>
2016-08-25 14:15:38 +0530

    Update knife.rb

--- chef-repo/.chef/knife.rb
+++ chef-repo/.chef/knife.rb
@@ -1,9 +1,16 @@
+current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
-node_name                'sumedh'
-client_key               '/home/devops/chef-repo/.chef/sumedh.pem'
-validation_client_name   'psl-validator'
-validation_key           '/home/devops/chef-repo/.chef/psl-validator.pem'
-chef_server_url          'https://10.51.237.155/organizations/psl'
-syntax_check_cache_path  '/home/devops/chef-repo/.chef/syntax_check_cache'
-cookbook_path [ '/home/devops/chef-repo/cookbooks' ]
+node_name                "sumedh"
+client_key               "#{current_dir}/sumedh.pem"
+chef_server_url          "https://10.51.237.155/organizations/psl"
+cookbook_path            ["#{current_dir}/../cookbooks"]
+validation_client_name   "persistent-validator"
+validation_key           "#{current_dir}/persistent-validator.pem"
+syntax_check_cache_path  "#/home/devops/chef-repo/.chef/syntax_check_cache"
+cookbook_path            ["/home/devops/chef-repo/cookbooks"]
+
+no_proxy                 'localhost,127.0.0.1,10.51.237.155,hj-ibmibm4486,local.home'
+http_proxy               'http://sumedh_surlekar:Titan1988@hjproxy.persistent.co.in:8080'
+https_proxy              'https://sumedh_surlekar:Titan1988@hjproxy.persistent.co.in:8080'
+ssl_verify_mode          :verify_none
