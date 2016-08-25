2f85665e70127969ee058b9ea916e8b2d72a8180
sumedh88 <sumedh_surlekar@persistent.com>
2016-08-25 11:51:15 +0530

    replace the .chef folder from my VM 10.51.237.155

--- chef-repo/.chef/knife.rb
+++ chef-repo/.chef/knife.rb
@@ -1,21 +1,9 @@
-# See https://docs.getchef.com/config_rb_knife.html for more information on knife configuration options
-
-current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
-node_name                "admin"
-client_key               "#{current_dir}/admin.pem"
-chef_server_url          "https://10.51.238.183/organizations/persistent"
-cookbook_path            ["#{current_dir}/../cookbooks"]
-validation_client_name   "persistent-validator"
-validation_key           "#{current_dir}/persistent-validator.pem"
-syntax_check_cache_path  "#/home/niloday/chef-repo/.chef/syntaxcache"
-cookbook_path            ["/home/niloday/chef-repo/cookbooks"]
-
-no_proxy                 '127.0.0.1,10.51.238.183,gadevopsnt2,*.persistent.com,*.persistent.co.in'
-http_proxy               'http://niloday_tamhankar:Dev0ps2016@ptproxy.persistent.co.in:8080'
-https_proxy              'https://niloday_tamhankar:Dev0ps2016@ptproxy.persistent.co.in:8080'
-ssl_verify_mode          :verify_none
-
-
-
+node_name                'sumedh'
+client_key               '/home/devops/chef-repo/.chef/sumedh.pem'
+validation_client_name   'psl-validator'
+validation_key           '/home/devops/chef-repo/.chef/psl-validator.pem'
+chef_server_url          'https://10.51.237.155/organizations/psl'
+syntax_check_cache_path  '/home/devops/chef-repo/.chef/syntax_check_cache'
+cookbook_path [ '/home/devops/chef-repo/cookbooks' ]
