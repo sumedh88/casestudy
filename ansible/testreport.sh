f7e18c04e117584d653cb23bfc464d587dac922e
sumedh88 <sumedh_surlekar@persistent.com>
2016-08-26 12:53:40 +0530

    last

--- ansible/testreport.sh
+++ ansible/testreport.sh
@@ -10,15 +10,15 @@ result=$(sed '$!d' test_result.log)
 #echo $result
 if [ "$result" = "OK" ];
 then
-  echo "OK" >> result.txt
+  echo "OK" > result.txt
 else
   failedTests=$(echo $result| grep -o -E '[0-9]+')
   if [ $(( $failedTests * 100 / 3)) -gt 10 ];
   then
         #echo $result;
-        echo "FAILED" >> result.txt
+        echo "FAILED" > result.txt
   else
         # echo $result;
-        echo "OK" >> result.txt
+        echo "OK" > result.txt
   fi
 fi
