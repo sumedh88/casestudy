02194e81c0df3514f184f78b4a51e1d63ac3a1e9
sumedh88 <sumedh_surlekar@persistent.com>
2016-08-27 14:43:47 +0530

    garbage code deleted 27 Aug

--- ansible/clusterScripts/datascript.sh
+++ ansible/clusterScripts/datascript.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 cd /root/mysql-cluster-gpl-7.4.12-linux-glibc2.5-x86_64
-=======
 cd mysql-cluster-gpl-7.4.12-linux-glibc2.5-x86_64
->>>>>>> dfaebcf1e156de199270127dbd023c844a850ac4
 cp bin/ndbd /usr/local/bin
 cd /usr/local/bin
 chmod +x ndb*
--- ansible/clusterScripts/mgmtscript.sh
+++ ansible/clusterScripts/mgmtscript.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 cd /root/mysql-cluster-gpl-7.4.12-linux-glibc2.5-x86_64
 cp bin/ndb_mgm* /usr/local/bin
 cd /usr/local/bin
 chmod +x ndb_mgm*
 mkdir -p /var/lib/mysql-cluster
-=======
 cd mysql-cluster-gpl-7.4.12-linux-glibc2.5-x86_64
 cp bin/ndb_mgm* /usr/local/bin
 cd /usr/local/bin
 chmod +x ndb_mgm*
 mkdir /var/lib/mysql-cluster
->>>>>>> dfaebcf1e156de199270127dbd023c844a850ac4
